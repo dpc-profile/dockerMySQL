e860b31ced803a8c65bb6a2f283cf298ee101706
dpc-profile <daniel.pcastro11@gmail.com>
2023-05-02 17:54:06 -0300

    limpando pasta db-docker

--- SQL/exemplos2Student.sql
+++
@@ -1,9 +0,0 @@
--- https://youtu.be/HXV3zeQKqGY?t=7716 #FreeCodeCamp Database course
-USE girafa;
-
-SELECT student.student_id, student.name, student.major
-FROM student
-WHERE student_id > 3 OR major = 'Biology'
--- WHERE name IN ('Claire', 'Kate', 'Mike')
-ORDER BY name
-LIMIT 5;
